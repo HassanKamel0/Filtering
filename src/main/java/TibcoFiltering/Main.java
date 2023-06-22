1b5ca07d3199afed7a833ed3eee0d0a3d68f4903
HassanKamel0 <hassankamel1998@gmail.com>
2023-06-22 18:02:12 +0300

    Adding try and catch to handle an arrays which have json objects inside

--- src/main/java/TibcoFiltering/Main.java
+++ src/main/java/TibcoFiltering/Main.java
@@ -1,31 +1,43 @@
 package TibcoFiltering;
 
-import java.io.FileReader;
+
 import java.io.IOException;
 import java.util.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ParseException {
-        String queryFields = "id,quoteID,notes,isBundled,requestedCompletionDate";
-        JSONObject response = (JSONObject) new JSONParser().parse(new FileReader("\\Users\\hkamel\\Downloads\\Submit.json"));
-        String returned= filteringResponse(queryFields, response);
-        System.out.println(returned);
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String prettyJson = gson.toJson(JsonParser.parseString(String.valueOf(returned)));
-        System.out.println(prettyJson);
+    public static void main(String[] args) throws IOException {
+        String queryFields = "Name";
+        String response = "{\"Name\": \"John\", \"id\": 30, \"href\": \"New York\"}";
+        String str = "[{\"No\":\"17\",\"Name\":\"Andrew\"},{\"No\":\"18\",\"Name\":\"Peter\"}, {\"No\":\"19\",\"Name\":\"Tom\"}]";
+        System.out.println(filteringResponse(queryFields,str));
+       // String returned= filteringResponse(queryFields, response);
+       // System.out.println(returned);
+//       String result= Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//        String prettyJson = gson.toJson(jsonObject);
+//        System.out.println(prettyJson);
     }
-    private static String filteringResponse(String queryFields,JSONObject response) throws JsonProcessingException {
+    public static String filteringResponse(String queryFields,String response2) throws JsonProcessingException {
         Map<String, Object> filteredResponse=new LinkedHashMap<>();
-        for (String field : queryFields.split(","))
-            filteredResponse.put(field,response.get(field));
+        try {
+            JSONArray array = new JSONArray(response2);
+            JSONArray output = new JSONArray();
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject object = array.getJSONObject(i);
+                output.put(new JSONObject(filteringResponse(queryFields, String.valueOf(object))));
+            }
+            return String.valueOf(output);
+        }catch (Exception e) {
+            JSONObject response = new JSONObject(response2);
+             filteredResponse.put("id",response.get("id"));
+             filteredResponse.put("href",response.get("href"));
+            if (!queryFields.isEmpty())
+                for (String field : queryFields.split(","))
+                    filteredResponse.put(field, response.get(field));
+        }
         return new ObjectMapper().writeValueAsString(filteredResponse);
     }
 }
