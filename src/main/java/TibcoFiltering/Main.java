01b7018341afbe6f4db78d09c46a4ec4ac5863fe
HassanKamel0 <hassankamel1998@gmail.com>
2023-06-26 12:33:40 +0300

    Fix the returned string from the method to retain the objects inside the json object

--- src/main/java/TibcoFiltering/Main.java
+++ src/main/java/TibcoFiltering/Main.java
@@ -6,13 +6,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String queryFields = "Name";
+        String queryFields = "isLocked,requester,attrs";
         String response = "{\"Name\": \"John\", \"id\": 30, \"href\": \"New York\"}";
         String str = "[{\"No\":\"17\",\"Name\":\"Andrew\"},{\"No\":\"18\",\"Name\":\"Peter\"}, {\"No\":\"19\",\"Name\":\"Tom\"}]";
-        System.out.println(filteringResponse(queryFields,str));
+        String s="{\"isLocked\": false, \"biSpecification\": null, \"requester\": \"vewali\", \"requestID\": null, \"quoteOn\": null, \"attrs\": [ { \"name\": \"orderCreateDateTime\", \"value\": \"2023-05-01T11:55:12.000+04:00\" }, { \"name\": \"CONTACTS_MODIFIED\", \"value\": \"True\" }, { \"name\": \"CL_CALLBACK_URL\", \"value\": \"http://dx10889:9090/processRequest\" }, { \"name\": \"ETISALAT_PROMOTIONAL_SMS\", \"value\": \"False\" }, { \"name\": \"ETISALAT_PROMOTIONAL_CALL\", \"value\": \"False\" }, { \"name\": \"channelUserFullName\", \"value\": \"Veeresh Wali\" }, { \"name\": \"channelUserEmailId\", \"value\": \"vewali@etisalat.ae\" }, { \"name\": \"transactionID\", \"value\": \"6a029a7a-b3d5-4b99-9293-0f6b3d5bad0c\" }, { \"name\": \"priority\", \"value\": \"10\" }, { \"name\": \"SUB_CHANNEL_CODE\", \"value\": \"BCRM\" }, { \"name\": \"orderType\", \"value\": \"NEW\" }, { \"name\": \"orderSubType\", \"value\": \"NEW\" } ], \"items\": [ { \"id\": \"http://localhost:8080/eoc/on/v1/sc/203665/pooi/24196503560\" }, { \"id\": \"http://localhost:8080/eoc/on/v1/sc/203665/pooi/24196503589\" }, { \"id\": \"http://localhost:8080/eoc/on/v1/sc/203665/pooi/24196503602\" }, { \"id\": \"http://localhost:8080/eoc/on/v1/sc/203665/pooi/24196503616\" }, { \"id\": \"http://localhost:8080/eoc/on/v1/sc/203665/pooi/24196503638\" }, { \"id\": \"http://localhost:8080/eoc/on/v1/sc/203665/pooi/24196503661\" } ], \"notes\": [ { \"text\": \"g\", \"author\": \"Veeresh Wali\" } ]}";
+        System.out.println(filteringResponse(queryFields,s));
     }
     public static String filteringResponse(String queryFields,String payload) throws JsonProcessingException {
         Map<String, Object> filteredResponse=new LinkedHashMap<>();
@@ -26,12 +28,12 @@ public class Main {
             return String.valueOf(arrayResponse);
         }catch (Exception e) {
             JSONObject response = new JSONObject(payload);
-             filteredResponse.put("id",response.get("id"));
-             filteredResponse.put("href",response.get("href"));
+//             filteredResponse.put("id",response.get("id"));
+//             filteredResponse.put("href",response.get("href"));
             if (!queryFields.isEmpty())
                 for (String field : queryFields.split(","))
                     filteredResponse.put(field, response.get(field));
         }
-        return new ObjectMapper().writeValueAsString(filteredResponse);
+        return new Gson().toJson(filteredResponse);
     }
 }
